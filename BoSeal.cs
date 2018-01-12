2894548bb15f9d426db881fbbb3a832e5766f62b
Bo <35234383+commandxr@users.noreply.github.com>
2018-01-12 17:43:44 -0600

    Add files via upload

--- BoSeal.cs
+++ BoSeal.cs
@@ -1,21 +1,28 @@
-         //Made by Bo
-		 //Give credits if you use this
-		 //Must have a raw pastebin link with the PC names of the users
-		 //If you have a question, feel free to contact me https://www.github.com/comandxr or instagram @commandxr
-		 //Don't leech
-         private void LoginButton_Click(object sender, EventArgs e)
+using System.Net;
+//<summary>
+//Made by Bo
+//Give credits if you use this
+//Must have a raw pastebin link with the PC names of the users
+//If you have a question, feel free to contact me https://www.github.com/comandxr or instagram @commandxr
+//Version 1.0
+//</summary>
+
+#region Authenticator
+private void Button_Click(object sender, EventArgs e)
         {
+            //Copy and Paste this code:
             string website = "Pastebin link here";
             string auth = (new WebClient()).DownloadString(website.ToString());
             if (auth.Contains(Environment.MachineName))
             {
                 MessageBox.Show("You're authenticated!");
-                tool tool = new tool();
-                tool.Show();
+                Form().Show();
                 this.Hide();
             }
             else
             {
                 MessageBox.Show("You're not authenticated!");
             }
+            //End of code
         }
+#endregion
